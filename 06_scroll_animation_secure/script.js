ea57ba4e5dac8842cebad53476e6bb838d6abbb2
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2021-01-08 21:34:06 +0300

    07_Split_Landing_Page

--- 06_scroll_animation_secure/script.js
+++ 06_scroll_animation_secure/script.js
@@ -2,16 +2,16 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
 
-checkBoxes();
+// checkBoxes();
 
 function checkBoxes() {
   // console.log(`viewport height: ${window.innerHeight}`, `trigger point height: ${window.innerHeight / 5 * 4}`);
   // the random trigger point
 
   const triggerBottom = window.innerHeight / 5 * 4;
+
   boxes.forEach(box => {
     const boxTop = box.getBoundingClientRect().top;
-
     // console.log(`the top of every box: ${boxTop}`);
 
     if(boxTop > triggerBottom) {
