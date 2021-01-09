6f2546d8ce8039931bf06a64ebf4449e72defa68
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2021-01-09 22:33:15 +0300

    08_Form_input_Wave

---
+++ 08_Form_input_Wave/script.js
@@ -0,0 +1,10 @@
+console.log('hi');
+
+const labels = document.querySelectorAll('.form-label');
+
+labels.forEach(label => {
+  label.innerHTML = label.innerText
+        .split('')
+        .map((letter, index) => `<span style="transition-delay: ${index * 50}ms;">${letter}</span>`)
+        .join('')
+});
