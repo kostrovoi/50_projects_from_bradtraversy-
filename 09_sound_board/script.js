2b3b9adf197fc4900aefa2b8262b4e82ccd86393
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2021-01-10 20:32:51 +0300

    09_sound_board

---
+++ 09_sound_board/script.js
@@ -0,0 +1,26 @@
+console.log('hi');
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'] ;
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  document.getElementById('buttons').appendChild(btn);
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+});
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
